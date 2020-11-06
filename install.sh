9287e5a6e95906f5117b3953782396f160722e06
postarc <45208608+postarc@users.noreply.github.com>
2020-11-06 21:51:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,43 +33,43 @@ cd ..
 ###############################################
 #Create /etc/ssmtp/ssmtp.conf
 echo -e "${CYAN}Creating configuration...${NC}"
-echo -e "root=$MAIL_ADDR" > /etc/ssmtp/ssmtp.conf
-echo 'mailhub=smtp.mail.ru' >> /etc/ssmtp/ssmtp.conf
-echo 'hostname=raspberry' >> /etc/ssmtp/ssmtp.conf
-echo 'UseTLS=YES' >> /etc/ssmtp/ssmtp.conf
-echo 'UseSTARTTLS=YES' >> /etc/ssmtp/ssmtp.conf
-echo 'AuthMethod=LOGIN' >> /etc/ssmtp/ssmtp.conf
-echo -e "AuthUser=$MAIL_ADDR" >> /etc/ssmtp/ssmtp.conf
-echo -n -e "${YELLOW}Input mail password:${NC}"
+sudo echo -e "root=$MAIL_ADDR" > /etc/ssmtp/ssmtp.conf
+sudo echo 'mailhub=smtp.mail.ru' >> /etc/ssmtp/ssmtp.conf
+sudo echo 'hostname=raspberry' >> /etc/ssmtp/ssmtp.conf
+sudo echo 'UseTLS=YES' >> /etc/ssmtp/ssmtp.conf
+sudo echo 'UseSTARTTLS=YES' >> /etc/ssmtp/ssmtp.conf
+sudo echo 'AuthMethod=LOGIN' >> /etc/ssmtp/ssmtp.conf
+sudo echo -e "AuthUser=$MAIL_ADDR" >> /etc/ssmtp/ssmtp.conf
+sudo echo -n -e "${YELLOW}Input mail password:${NC}"
 read PASSWD
-echo -e "AuthPass=$PASSWD" >> /etc/ssmtp/ssmtp.conf    #a27TLxmqGdqgJ7N           
-echo 'FromLineOverride=NO' >> /etc/ssmtp/ssmtp.conf
+sudo echo -e "AuthPass=$PASSWD" >> /etc/ssmtp/ssmtp.conf    #a27TLxmqGdqgJ7N           
+sudo echo 'FromLineOverride=NO' >> /etc/ssmtp/ssmtp.conf
 ##################################################
 #Create /etc/ssmtp/revaliases
-echo -e "root:$MAIL_ADDR" > /etc/ssmtp/revaliases
-echo -e "pi:raspberry_pi@bk.ru" >> /etc/ssmtp/revaliases
+sudo echo -e "root:$MAIL_ADDR" > /etc/ssmtp/revaliases
+sudo echo -e "pi:raspberry_pi@bk.ru" >> /etc/ssmtp/revaliases
 ##################################################
 
 echo -e -n "${YELLOW}Input your workgroup name:${NC}"
 read WORKGROUP_N
 sed -i "s/.*workgroup =.*/workgroup = ${WORKGROUP_N}/" /etc/samba/smb.conf
 
-echo "[share_pi]" >> /etc/samba/smb.conf
-echo " comment=Raspberry Pi Share" >> /etc/samba/smb.conf
-echo " path=/home/pi/share" >> /etc/samba/smb.conf
-echo " browseable=Yes" >> /etc/samba/smb.conf
-echo " writeable=Yes" >> /etc/samba/smb.conf
-echo " guest ok =Yes" >> /etc/samba/smb.conf
-echo " create mask=0777" >> /etc/samba/smb.conf
-echo " directory mask=0777" >> /etc/samba/smb.conf
-service smbd restart
+sudo echo "[share_pi]" >> /etc/samba/smb.conf
+sudo echo " comment=Raspberry Pi Share" >> /etc/samba/smb.conf
+sudo echo " path=/home/pi/share" >> /etc/samba/smb.conf
+sudo echo " browseable=Yes" >> /etc/samba/smb.conf
+sudo echo " writeable=Yes" >> /etc/samba/smb.conf
+sudo echo " guest ok =Yes" >> /etc/samba/smb.conf
+sudo echo " create mask=0777" >> /etc/samba/smb.conf
+sudo echo " directory mask=0777" >> /etc/samba/smb.conf
+sudo service smbd restart
 ####################################################
 MAC_ADDR=$(cat /sys/class/net/eth0/address)
 sed -i "s/.*test_subj=.*/test_subj=\"mac=${MAC_ADDR};vpn=192.169.3.3\"/" processmail_sh1
 
 echo -e "${CYAN}Copy files...${NC}"
 chmod +x Current-Map_.rep mail_sh2 on_off_sh2 on_reboot_sh2 onewire_sh1 processmail_sh1 supervise_sh1 thermo_sh1 Trends_ddp.rep
-mv Current-Map_.rep mail_sh2 on_off_sh2 on_reboot_sh2 onewire_sh1 processmail_sh1 supervise_sh1 thermo_sh1 Trends_ddp.rep /usr/local/bin/
+sudo mv Current-Map_.rep mail_sh2 on_off_sh2 on_reboot_sh2 onewire_sh1 processmail_sh1 supervise_sh1 thermo_sh1 Trends_ddp.rep /usr/local/bin/
 
 sudo /usr/local/bin/supervise_sh1 &
 cd ..
